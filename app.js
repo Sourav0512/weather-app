76528069fda4d64d11c3a9cf9ae1f6240fd4c0de
“Sourav <sourav.das@gilbarco.com>
2018-03-29 17:58:41 +0530

    commit lat and lng

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ request({
             json : true
         },
         (error, response, body) => {
-             console.log('error:', error); // Print the error if one occurred
-             console.log(JSON.stringify(response,undefined,4)); // Print the response status code if a response was received
-             console.log('==================================');
-             console.log(JSON.stringify(body,undefined,4)); // Print the HTML for the Google homepage.
+             // console.log('error:', error); // Print the error if one occurred
+             // console.log(JSON.stringify(response,undefined,4)); // Print the response status code if a response was received
+             // console.log('==================================');
+             // console.log(JSON.stringify(body,undefined,4)); // Print the HTML for the Google homepage.
+             console.log(`latitude  : ${body.results[0].geometry.location.lat}`);
+             console.log(`longitude : ${body.results[0].geometry.location.lng}`);
         });
