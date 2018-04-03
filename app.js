8559e893ce50ad9e59332cf514edae6791a72ea1
“Sourav <sourav.das@gilbarco.com>
2018-04-03 16:30:55 +0530

    commits weather-app changes

--- app.js
+++ app.js
@@ -2,19 +2,62 @@ console.log('firing up app.js');
 const request = require('request');
 const yargs = require('yargs');
 
+
+// const argv = yargs.
+//             options(
+//               {
+//                 address : {
+//                              demand : true,
+//                              alias : 'a',
+//                              describe : 'address to fetch weather for',
+//                              string : true
+//                           }
+//               }
+//             )
+//             .help()
+//             .alias('help','h')
+//             .argv;
+
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+
 const argv = yargs
-            .command('address','enter specific address',
-            {
-              alias : 'a',
-              demand : true,
-              title :
+            .command('address','address to fetch info for',undefined)
+            // (address) =>
+            // {
+            //   demand : true,
+            //   string : true,
+            //    alias : 'a'
+            // })
+            .options({
+              address: {
+                demand: true,
+                alias: 'a',
+              describe: 'enter Address here',
+                // string: true
+                }
             })
-            .help()
-            .argv;
+          .help()
+          .alias('help','h')
+          .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
+console.log(argv);
 
 // the first param is options object and the second param is a callback
 request({
-            url : 'http://maps.googleapis.com/maps/api/geocode/json?address=Premier%20residencies',
+            url : `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
             json : true
         },
         (error, response, body) => {
@@ -22,6 +65,8 @@ request({
              // console.log(JSON.stringify(response,undefined,4)); // Print the response status code if a response was received
              // console.log('==================================');
              // console.log(JSON.stringify(body,undefined,4)); // Print the HTML for the Google homepage.
+             debugger;
+             console.log(`address : ${body.results[0].formatted_address}`);
              console.log(`latitude  : ${body.results[0].geometry.location.lat}`);
              console.log(`longitude : ${body.results[0].geometry.location.lng}`);
         });
