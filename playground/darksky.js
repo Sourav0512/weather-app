09801e57174ad9896aab2b8f49deae75f6c3b3dd
Sourav Das <05sourav@gmail.com>
2018-04-05 02:42:04 +0530

    wiring multiple callbacks together

--- playground/darksky.js
+++ playground/darksky.js
@@ -1,18 +1,29 @@
 // secret key darksky api - 5bf18c4ec1eacd30f41fef1facacbfb0
+// 19.0795697,42.887732224234
 console.log('firing up darksky.js');
 const request = require('request');
+const key = '5bf18c4ec1eacd30f41fef1facacbfb0';
 
-request({
-  url: 'https://api.darksky.net/forecast/5bf18c4ec1eacd30f41dfef1facacbfb0/19.0795697,42.887732224234',
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Forecast.io server.');
-  } else if (response.statusCode === 400) {
-    console.log(response.error);
-  } else if (response.statusCode === 200) {
-    console.log(body.currently.temperature);
-  }else {
-    console.log('forbidden');
-  }
-});
+var fetchWeather = (longitude,latitude,callback) => {
+
+              request({
+                url: `https://api.darksky.net/forecast/${key}/${longitude},${latitude}`,
+                json: true
+              }, (error, response, body) => {
+                if (error) {
+                  callback('Unable to connect to Forecast.io server.');
+                } else if (response.statusCode === 400) {
+                  callback(response.error);
+                } else if (response.statusCode === 200) {
+                  callback(undefined,{
+                    temperature : body.currently.temperature,
+                    apparentTemperature : body.currently.apparentTemperature
+                  })
+                }else {
+                  console.log('forbidden');
+                }
+              });
+
+            };
+
+module.exports.fetchWeather = fetchWeather;
