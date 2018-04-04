66c509f13d54d71b8212219c8384589d5a504e8c
“Sourav <sourav.das@gilbarco.com>
2018-04-04 13:57:25 +0530

    Changes to be committed: 	modified:   app.js 	new file:   geocode/geocode.js

--- app.js
+++ app.js
@@ -1,72 +1,28 @@
 console.log('firing up app.js');
-const request = require('request');
 const yargs = require('yargs');
-
-
-// const argv = yargs.
-//             options(
-//               {
-//                 address : {
-//                              demand : true,
-//                              alias : 'a',
-//                              describe : 'address to fetch weather for',
-//                              string : true
-//                           }
-//               }
-//             )
-//             .help()
-//             .alias('help','h')
-//             .argv;
-
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
+const geocode = require('./geocode/geocode.js');
 
 const argv = yargs
-            .command('address','address to fetch info for',undefined)
-            // (address) =>
-            // {
-            //   demand : true,
-            //   string : true,
-            //    alias : 'a'
-            // })
+            .command('address','address to fetch info for',{string:true})
             .options({
-              address: {
+              address:
+               {
                 demand: true,
                 alias: 'a',
-              describe: 'enter Address here',
-                // string: true
-                }
+                describe: 'enter Address here',
+               }
             })
           .help()
           .alias('help','h')
           .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-console.log(argv);
-
-// the first param is options object and the second param is a callback
-request({
-            url : `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-            json : true
-        },
-        (error, response, body) => {
-             // console.log('error:', error); // Print the error if one occurred
-             // console.log(JSON.stringify(response,undefined,4)); // Print the response status code if a response was received
-             // console.log('==================================');
-             // console.log(JSON.stringify(body,undefined,4)); // Print the HTML for the Google homepage.
-             debugger;
-             console.log(`address : ${body.results[0].formatted_address}`);
-             console.log(`latitude  : ${body.results[0].geometry.location.lat}`);
-             console.log(`longitude : ${body.results[0].geometry.location.lng}`);
+geocode.processAddress(argv.address,(error,results)=>
+        {
+          if(error && (results === undefined)){
+            console.log(error);
+          }
+          else {
+            console.log(`Address below ===> `);
+            console.log(`weather : ${JSON.stringify(results,undefined,4)}`);
+          }
         });
---
+++ geocode/geocode.js
@@ -0,0 +1,29 @@
+console.log('firing up geocode.js');
+const request = require('request');
+
+
+var processAddress = (address,callback) =>
+            {
+              var encodedAddress = encodeURIComponent(address);
+
+              request({
+                          url : `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+                          json : true
+                      },
+                      (error, response, body) => {
+                          if(error)
+                          {
+                            callback('something wrong with server ',undefined);
+                          }else if (body.status === 'ZERO_RESULTS') {
+                            callback('invalid results',undefined);
+                          }else if (body.status === 'OK') {
+                            callback(undefined,{
+                              address  : body.results[0].formatted_address,
+                              latitude : body.results[0].geometry.location.lat,
+                              longitude: body.results[0].geometry.location.lng
+                            });
+                          }
+                      });
+            };
+
+module.exports = {processAddress};
