87dbe93451396d2e6afbe121d6bde704959d3346
Sourav Das <05sourav@gmail.com>
2018-04-05 19:41:24 +0530

    promise code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log('firing up app.js');
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode.js');
 const darksky = require('./playground/darksky.js');
+const promise2 = require('./playground/promise-2.js');
 
 const argv = yargs
             .command('address','address to fetch info for',{string:true})
@@ -17,21 +18,34 @@ const argv = yargs
           .alias('help','h')
           .argv;
 
-geocode.processAddress(argv.address,(error,results)=>
-        {
-          if(error && (results === undefined)){
-            console.log(error);
-          }
-          else {
-            darksky.fetchWeather(results.longitude,
-                                 results.latitude,
-                                 (error,weatherResults) => {
-                                   if (error) {
-                                    console.log(error);
-                                   } else {
-                                     console.log(`weather is still ${weatherResults.temperature}.It is actually  ${weatherResults.apparentTemperature}.`);
-                                   }
-                                 });
-            console.log(`address is at ${JSON.stringify(results.address,undefined,4)}`);
-          }
-        });
+// geocode.processAddress(argv.address,(error,results)=>
+//         {
+//           if(error && (results === undefined)){
+//             console.log(error);
+//           }
+//           else {
+//             darksky.fetchWeather(results.longitude,
+//                                  results.latitude,
+//                                  (error,weatherResults) => {
+//                                    if (error) {
+//                                     console.log(error);
+//                                    } else {
+//                                      console.log(`weather is still ${weatherResults.temperature}.It is actually  ${weatherResults.apparentTemperature}.`);
+//                                    }
+//                                  });
+//             console.log(`address is at ${JSON.stringify(results.address,undefined,4)}`);
+//           }
+//         });
+
+// returning promise
+
+promise2.processAddress(argv.address)
+         .then((result) =>{
+          console.log(JSON.stringify(result,undefined,4));
+          darksky.fetchWeather(result.longitude,result.latitude,
+                (error,weatherResults) => {
+                            if (error) {
+                            console.log(error);
+                            } else {
+                            console.log(`weather is still ${weatherResults.temperature}.It is actually  ${weatherResults.apparentTemperature}.`);
+                          }})},(error)=>{console.log(`error : ${error}`);});
